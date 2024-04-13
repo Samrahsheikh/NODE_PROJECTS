3613f8c077d6c7954652fb70bc188debd4c00c53
Samrahsheikh <159701577+Samrahsheikh@users.noreply.github.com>
2024-04-14 00:25:34 +0500

    Add files via upload

---
+++ 22_International_Error/International_Error.js
@@ -0,0 +1,9 @@
+"use strict";
+// If you haven't received an array index error in one of your programs yet, try to make one heppen.
+// Creating array
+let newArray = ['Samrah', 'Mishal', 'Taha'];
+// Accessing array by index
+console.log(newArray[4]);
+// Make sure you correct the error before closing the program.
+// Correct the error
+console.log(newArray);
---
+++ 22_International_Error/International_Error.ts
@@ -0,0 +1,15 @@
+// Question # 22
+
+// If you haven't received an array index error in one of your programs yet, try to make one happen.
+
+// Creating array
+let newArray :string[] = ['Samrah','Mishal','Taha'];
+
+// Accessing array by index
+console.log(newArray[4]);
+
+// Make sure you correct the error before closing the program.
+
+// Correct the error
+console.log(newArray);
+
