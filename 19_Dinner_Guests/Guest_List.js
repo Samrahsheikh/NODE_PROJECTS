164fa5c6d7afb19494f012fd6b89e1efab4e938b
Samrahsheikh <159701577+Samrahsheikh@users.noreply.github.com>
2024-04-14 00:30:57 +0500

    Add files via upload

---
+++ 19_Dinner_Guests/Guest_List.js
@@ -0,0 +1,42 @@
+"use strict";
+//If you could invite anyone, living or deceased, to dinner.
+let Guest_List = ['Ashna Syed', 'Mehreen Mujeeb', 'Romesa Arif'];
+// for (let i=0; i<Guest_List.length; i++){
+//     console.log ('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n')
+// }
+//You just heard that one of your guests can't make the dinner, 
+let absent_Guest = 'Ashna Syed';
+//so you need to send out a new set of invitations.
+let new_Guest = 'Javeria Shaikh';
+Guest_List[0] = new_Guest;
+// for (let i=0; i<Guest_List.length; i++){
+//     console.log ('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n')
+// }
+console.log(`Ms ${absent_Guest} is not coming to the party,`);
+console.log('Good News! We find Big table so we are inviting 3 more guests.');
+// array mai 3 guest add kiye hein.
+Guest_List.unshift('Fabiha');
+Guest_List.splice(2, 0, 'Ghania Sheikh');
+Guest_List.push('Kinza Sheikh');
+// yahan per mane 6 arry k guest ko print karwaya hai.
+// for (let i=0; i<Guest_List.length; i++){
+// console.log ('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n')
+// }
+// Sorry message to guest for not inviting.
+// console.log('\nSorry we can not arrange big table, Only Two peoples will be invited.');
+// Yahan per mane guest remove kiye hein.
+while (Guest_List.length > 2) {
+    let remove_Guest = Guest_List.pop();
+    // console.log(`Sorry Ms. ${remove_Guest}, You are not invited for Dinner.`);
+}
+// Hamary bache hoye 2 invited guest.
+// for (let i=0; i<Guest_List.length; i++){
+//     console.log ('Dear Ms. ' + Guest_List[i] + ',\n\nYou are still invited.\n\nThank You!\n\n')
+// }
+// mane sary guest arry se remove kardiye.
+Guest_List.splice(0, 2);
+console.log(Guest_List);
+console.log('Best Regards: Samrah sheikh');
+// Exercise 19
+// print a message indicating the number of people you are inviting to dinner.
+console.log(`Total number of Guest Are: ${Guest_List.length}`);
