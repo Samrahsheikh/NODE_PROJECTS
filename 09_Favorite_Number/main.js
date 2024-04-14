858133eea9f719b900723a31e547ff3c0cec39f9
Samrahsheikh <159701577+Samrahsheikh@users.noreply.github.com>
2024-04-14 15:23:39 +0500

    Add files via upload

---
+++ 09_Favorite_Number/main.js
@@ -0,0 +1,3 @@
+var favnumber = 7;
+var message = "My Favorite Number is:";
+console.log(message, favnumber);
---
+++ 09_Favorite_Number/main.ts
@@ -0,0 +1,11 @@
+// Question # 09
+
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+// create a message that reveals your favorite number. Print that message.
+
+
+let favnumber = 7;
+let message = "My Favorite Number is:";
+console.log(message, favnumber);
+
+
