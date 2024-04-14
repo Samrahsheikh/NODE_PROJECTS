8905dcb1e761902b52b55c1f25d23cdef785b3e9
Samrahsheikh <159701577+Samrahsheikh@users.noreply.github.com>
2024-04-14 15:21:18 +0500

    Add files via upload

---
+++ 13_Your_Own_Array/Your_Own_Array.js
@@ -0,0 +1,5 @@
+"use strict";
+let transportation = ['motorcycle', 'car', 'Train', 'Cycle'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log(`I would like to own a ${transportation[i]}.`);
+}
