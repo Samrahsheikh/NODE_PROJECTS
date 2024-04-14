a70e7bd0e342d4a051ab775de84c4d1e24bf8d7a
Samrahsheikh <159701577+Samrahsheikh@users.noreply.github.com>
2024-04-14 13:55:06 +0500

    Delete 14_Guest_List directory

--- 14_Guest_List/Guest_List.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//If you could invite anyone, living or deceased, to dinner.
-let Guest_List = ['Ashna Syed', 'Mehreen Mujeeb', 'Romesa Arif'];
-for (let i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!');
-}
