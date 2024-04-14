abcd63f72b35ff8ad8031f8b885571bdf6fa0a99
Samrahsheikh <159701577+Samrahsheikh@users.noreply.github.com>
2024-04-14 14:10:12 +0500

    Delete Your_Own_Array.js

--- Your_Own_Array.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let transportation = ['motorcycle', 'car', 'Train', 'Cycle'];
-for (let i = 0; i < transportation.length; i++) {
-    console.log(`I would like to own a ${transportation[i]}.`);
-}
