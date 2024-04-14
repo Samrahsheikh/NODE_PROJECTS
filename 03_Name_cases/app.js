8a6c394fa89105a7e8727985b9329a4a9ec10e52
Samrahsheikh <159701577+Samrahsheikh@users.noreply.github.com>
2024-04-14 15:26:04 +0500

    Add files via upload

---
+++ 03_Name_cases/app.js
@@ -0,0 +1,4 @@
+var personName = ("Samrah Sheikh");
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
