5ffa264f19e5a41916e5dd19f479e3ce7289aa77
Samrahsheikh <159701577+Samrahsheikh@users.noreply.github.com>
2024-04-14 15:22:07 +0500

    Add files via upload

---
+++ 12_Greeting/Name.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Samrah', 'Mehru', 'Iqra', 'Romi'];
+const message = 'Welcome to typscript class;';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12_Greeting/Name.ts
@@ -0,0 +1,16 @@
+// Question # 12
+
+
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. 
+// The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names :string[] = ['Samrah','Mehru','Iqra','Romi'];
+
+const message :string = 'Welcome to typscript class;'
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]);
+
+};
+
